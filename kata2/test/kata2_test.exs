630ff1a76fcb530ca58b7a9a282eac9edb89ff4b
Nizar Venturini <nizar.venturini@gmail.com>
2014-07-14 13:35:21 +0100

    Add tests

--- kata2/test/kata2_test.exs
+++ kata2/test/kata2_test.exs
@@ -1,7 +1,82 @@
 defmodule KataTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+
+  test "it works" do
+    assert -1 == Kata.Kata2.chop(3, [])
+  end
+
+  test "it works" do
+    assert -1 == Kata.Kata2.chop(3, [1])
+  end
+
+  test "it works" do
+    assert 0 ==  Kata.Kata2.chop(1, [1])
+  end
+  #
+
+  test "it works" do
+    assert 0 ==  Kata.Kata2.chop(1, [1, 3, 5])
+  end
+
+  test "it works" do
+    assert 1 ==  Kata.Kata2.chop(3, [1, 3, 5])
+  end
+
+  test "it works" do
+    assert 2 ==  Kata.Kata2.chop(5, [1, 3, 5])
+  end
+
+  test "it works" do
+    assert -1 == Kata.Kata2.chop(0, [1, 3, 5])
+  end
+
+  test "it works" do
+    assert -1 == Kata.Kata2.chop(2, [1, 3, 5])
+  end
+
+  test "it works" do
+    assert -1 == Kata.Kata2.chop(4, [1, 3, 5])
+  end
+
+  test "it works" do
+    assert -1 == Kata.Kata2.chop(6, [1, 3, 5])
+  end
+  #
+
+  test "it works" do
+    assert 0 ==  Kata.Kata2.chop(1, [1, 3, 5, 7])
+  end
+
+  test "it works" do
+    assert 1 ==  Kata.Kata2.chop(3, [1, 3, 5, 7])
+  end
+
+  test "it works" do
+    assert 2 ==  Kata.Kata2.chop(5, [1, 3, 5, 7])
+  end
+
+  test "it works" do
+    assert 3 ==  Kata.Kata2.chop(7, [1, 3, 5, 7])
+  end
+
+  test "it works" do
+    assert -1 == Kata.Kata2.chop(0, [1, 3, 5, 7])
+  end
+
+  test "it works" do
+    assert -1 == Kata.Kata2.chop(2, [1, 3, 5, 7])
+  end
+
+  test "it works" do
+    assert -1 == Kata.Kata2.chop(4, [1, 3, 5, 7])
+  end
+
+  test "it works" do
+    assert -1 == Kata.Kata2.chop(6, [1, 3, 5, 7])
+  end
+
+  test "it works" do
+    assert -1 == Kata.Kata2.chop(8, [1, 3, 5, 7])
   end
 end

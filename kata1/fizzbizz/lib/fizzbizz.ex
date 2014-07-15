aeee8a7b087ec84c61d0a9383ac0c6cdd55dfb3f
Nizar Venturini <nizar.venturini@gmail.com>
2014-07-15 14:37:45 +0100

    Use guards to solve same kata

--- kata1/fizzbizz/lib/fizzbizz.ex
+++ kata1/fizzbizz/lib/fizzbizz.ex
@@ -13,4 +13,9 @@ defmodule Kata.FizzBuzz do
     end
   end
 
+  def calc2(input) when rem(input,15) == 0, do: 'fizzbuzz'
+  def calc2(input) when rem(input, 5) == 0, do: 'buzz'
+  def calc2(input) when rem(input, 3) == 0, do: 'fizz'
+  def calc2(input) when true, do: input
+
 end
--- kata1/fizzbizz/test/fizzbizz_test.exs
+++ kata1/fizzbizz/test/fizzbizz_test.exs
@@ -32,4 +32,38 @@ defmodule KataTest do
   test "the truth" do
     assert 1 + 1 == 2
   end
+
+  #calc2
+
+  test "calc2 => input 1 gives 1" do
+    assert Kata.FizzBuzz.calc2(1) == 1
+  end
+
+  test "calc2 => input 3 gives fizz" do
+    assert Kata.FizzBuzz.calc2(3) == 'fizz'
+  end
+
+  test "calc2 => input 9 gives fizz" do
+    assert Kata.FizzBuzz.calc2(9) == 'fizz'
+  end
+
+  test "calc2 => input 10 gives buzz" do
+    assert Kata.FizzBuzz.calc2(10) == 'buzz'
+  end
+
+  test "calc2 => input 5 gives buzz" do
+    assert Kata.FizzBuzz.calc2(5) == 'buzz'
+  end
+
+  test "calc2 => input 15 gives buzz" do
+    assert Kata.FizzBuzz.calc2(15) == 'fizzbuzz'
+  end
+
+  test "calc2 => input 150 gives buzz" do
+    assert Kata.FizzBuzz.calc2(150) == 'fizzbuzz'
+  end
+
+  test "calc2 => the truth" do
+    assert 1 + 1 == 2
+  end
 end

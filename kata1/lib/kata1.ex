bc0e7d5ae5aa1e88f67ba6b074f5294ea468f95e
Nizar Venturini <nizar.venturini@gmail.com>
2014-08-05 13:00:03 +0100

    Add solution for Kata1

--- kata1/lib/kata1.ex
+++ kata1/lib/kata1.ex
@@ -1,6 +1,21 @@
 defmodule Kata.Kata1 do
 
   def calc(input) do
+    cond do
+      rem(input,15) == 0 ->
+        'fizzbuzz'
+      rem(input,5) == 0 ->
+        'buzz'
+      rem(input,3) == 0 ->
+        'fizz'
+      true ->
+        input
+    end
   end
+
+  def calc_with_guards(input) when rem(input,15) == 0, do: 'fizzbuzz'
+  def calc_with_guards(input) when rem(input, 5) == 0, do: 'buzz'
+  def calc_with_guards(input) when rem(input, 3) == 0, do: 'fizz'
+  def calc_with_guards(input) when true, do: input
 
 end
--- kata1/test/kata1_test.exs
+++ kata1/test/kata1_test.exs
@@ -29,4 +29,33 @@ defmodule KataTest do
     assert Kata.Kata1.calc(150) == 'fizzbuzz'
   end
 
+  #same but with guards
+  test "guards => input 1 gives 1" do
+    assert Kata.Kata1.calc_with_guards(1) == 1
+  end
+
+  test "guards => input 3 gives fizz" do
+    assert Kata.Kata1.calc_with_guards(3) == 'fizz'
+  end
+
+  test "guards => input 9 gives fizz" do
+    assert Kata.Kata1.calc_with_guards(9) == 'fizz'
+  end
+
+  test "guards => input 10 gives buzz" do
+    assert Kata.Kata1.calc_with_guards(10) == 'buzz'
+  end
+
+  test "guards => input 5 gives buzz" do
+    assert Kata.Kata1.calc_with_guards(5) == 'buzz'
+  end
+
+  test "guards => input 15 gives buzz" do
+    assert Kata.Kata1.calc_with_guards(15) == 'fizzbuzz'
+  end
+
+  test "guards => input 150 gives buzz" do
+    assert Kata.Kata1.calc_with_guards(150) == 'fizzbuzz'
+  end
+
 end
